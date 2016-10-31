418fa309f7ebb2ded0a182a711bc05342991f503
sddz_yuxiaohua <sddz_yuxiaohua@163.com>
2016-10-31 16:36:07 +0800

    remove testing code

--- server/main.js
+++ server/main.js
@@ -1,17 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 
-function greetMe(user) {
-  console.log(variable);
-  var variable = 1;
-  console.log(variable);
-  return 'Hi, ' + user;
-}
-
 Meteor.startup(() => {
   // code to run on server at startup
-
-  console.log(greetMe('hzyuxiaohua'));
-
   var apn = require('apn');
   console.log(apn);
 });
