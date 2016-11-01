409e551c818e999290154fd45871642f9486e978
sddz_yuxiaohua <sddz_yuxiaohua@163.com>
2016-11-01 10:19:18 +0800

    添加setState()方法的注释

--- client/components/clock.jsx
+++ client/components/clock.jsx
@@ -19,6 +19,16 @@ export default class Clock extends Component {
     });
   }
 
+  /*
+  * 不可以直接更新state，如：this.state.date = new Date();
+  * 正确的更新姿势为：this.setState({ date: new Date() });
+  * state的更新操作为异步操作，出于性能的考量会将多个update操作放在一起操作；
+  * 所以，props和state的更新时，不可以信赖彼此的状态，可以使用如下方式：
+  * this.setState((prevState, props) => {
+  *   counter: prevState.counter + props.increments
+  * });
+  */
+
   componentWillUnmount() {
     console.log('Clock Component will be unmounted.');
   }
