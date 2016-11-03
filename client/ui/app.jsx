08de88e51ce3a1d841acdc53b2e4108c7cbe62a2
sddz_yuxiaohua <sddz_yuxiaohua@163.com>
2016-11-03 19:18:44 +0800

    remove testing code

--- client/ui/app.jsx
+++ client/ui/app.jsx
@@ -14,11 +14,3 @@ export default class App extends React.Component {
     );
   }
 }
-
-/*
-
-{
-    "body":"{"aps":{"alert":"钦南\nii","badge":1,"category":"readAndDeletePushIdentifier","mutable-content":1,"sound":"testSound"},"p":{"fid":1,"m":"30:1tbiHgn+cFSISIpMbwABsl","type":"mail","u":"youxiangtest123456@163.com"}}",
-    "token":"11685452986B66473BC7B80A7DE39BD73E7FD7CDE818CDA048E6A6AEE5B6C953"
-}
-*/
