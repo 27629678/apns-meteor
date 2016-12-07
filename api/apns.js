a947eb38025ed20826f33f6d9a4c9e285c961073
sddz_yuxiaohua <sddz_yuxiaohua@163.com>
2016-12-07 17:43:20 +0800

    添加静默推送支持

--- api/apns.js
+++ api/apns.js
@@ -2,8 +2,8 @@ export default class APNS {
   constructor() {
     var options = {
       token: {
-        key: '/Users/APNsAuthKey_H45CDV34VF.p8',
-        keyId: 'H45CDV34VF',
+        key: '/Users/APNsAuthKey_JCYGYK5MLP.p8',
+        keyId: 'JCYGYK5MLP',
         teamId:'K8WE64BK54',
       },
       production: false,
@@ -21,6 +21,14 @@ export default class APNS {
       msg: {type: String},
       badge:{type: Number}
     }).validate({token, msg, badge});
+
+    var silent = new this.apn.Notification();
+    silent.topic = bundle_id;
+    silent.contentAvailable = true;
+
+    this.provider.send(silent, token).then((result) => {
+      console.log(result);
+    });
 
     // console.log('T: ' + token + ', M: ' + msg + ' B: ' + badge);
     var noti = new this.apn.Notification();
@@ -45,6 +53,7 @@ export default class APNS {
     promise.then((result) => {
       console.log(result);
     })
+
     // var resp = Meteor.wrapAsync(this.provider.send(noti, token).then);
     // resp(function (error, result) {
     //   console.log(result);
